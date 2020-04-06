9a8fc0e43230f92b38f40a314d24dd31354816ae
Morganhtrotter <morganhtrotter@gmail.com>
2020-04-06 13:12:05 -0700

    Assignment 1 rework

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -11,18 +11,69 @@ class DishDetail extends Component {
 	}
 
 	render() {
-		return(
-			<div>
-				<Card key={this.props.dish.id} >
-	        <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-	        <CardBody>
-	          <CardTitle>{this.props.dish.name}</CardTitle>
-	          <CardText>{this.props.dish.description}</CardText>
-	        </CardBody>
-	      </Card>
-      </div>
-		);
+		if (this.props.dish != null) {
+			return(
+			<div className="row">
+				{this.renderDish(this.props.dish)}
+	      		{this.renderComments(this.props.dish)}
+		    </div>
+			);
+		} else {
+			return(
+				<div></div>
+			);
+		}
 	}
+
+	renderDish(dish) {
+		if (dish != null) {
+			return(
+				<div className="col-12 col-md-5 m-1">
+					<Card key={this.props.dish.id} >
+			        <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+			        <CardBody>
+			          <CardTitle>{this.props.dish.name}</CardTitle>
+			          <CardText>{this.props.dish.description}</CardText>
+			        </CardBody>
+			      </Card>
+		      	</div>
+	      	);
+		} else {
+			return(
+				<div></div>
+			);
+		}
+	}
+
+	renderComments(dish) {
+      console.log(dish);
+      var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+      if (dish != null)
+          if (dish.comments != null) {
+            const listComments = dish.comments.map((comments) => 
+              <li>{comments.comment}<br /><br />{"--" + comments.author 
+                  + " , "
+                  + months[(new Date(comments.date)).getMonth()] + " "
+                  + (new Date(comments.date)).getDate() + ", "
+                  + (new Date(comments.date)).getFullYear()}<br /><br /></li>
+            );
+            return(
+              <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                  <ul className="list-unstyled">{listComments}</ul>
+              </div>
+            );
+          } else {
+            return(
+              <div></div>
+            );
+          }
+      else
+          return(
+            <div></div>
+          );
+    }
 }
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -33,35 +33,6 @@ class Menu extends Component {
             );
     }
 
-    renderComments(dish) {
-      console.log(dish);
-      var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-      if (dish != null)
-          if (dish.comments != null) {
-            const listComments = dish.comments.map((comments) => 
-              <li>{comments.comment}<br /><br />{"--" + comments.author 
-                  + " , "
-                  + months[(new Date(comments.date)).getMonth()] + " "
-                  + (new Date(comments.date)).getDate() + ", "
-                  + (new Date(comments.date)).getFullYear()}<br /><br /></li>
-            );
-            return(
-              <div>
-                <h4>Comments</h4>
-                  <ul className="list-unstyled">{listComments}</ul>
-              </div>
-            );
-          } else {
-            return(
-              <div></div>
-            );
-          }
-      else
-          return(
-            <div></div>
-          );
-    }
 
     render() {
         const menu = this.props.dishes.map((dish) => {
@@ -83,12 +54,12 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
+                <div>
+                  <div>
                     {this.renderDish(this.state.selectedDish)}
                   </div>
-                  <div className="col-12 col-md-5 m-1">
-                    {this.renderComments(this.state.selectedDish)}
+                  <div>
+                    <DishDetail selectedDish={this.state.selectedDish} />
                   </div>
                 </div>
             </div>
