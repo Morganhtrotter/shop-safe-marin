28328b4090f53e8d071b4fb907cac306658b0e9b
Morganhtrotter <morganhtrotter@gmail.com>
2020-04-08 11:43:45 -0700

    start of week 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-import DishDetail from './DishDetailComponent';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state = {
-            selectedDish: null
-        }
-        console.log('Menu Component constructor is invoked');
     }
 
     componentDidMount() {
@@ -22,24 +16,13 @@ class Menu extends Component {
         this.setState({ selectedDish: dish});
     }
 
-    renderDish(dish) {
-        if (dish != null)
-            return(
-                <DishDetail dish={dish} />
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
-
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div className="col-12 col-md-5 m-1">
                 <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
+                  onClick={() => this.props.onClick(dish.id)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
@@ -53,14 +36,6 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu}
-                </div>
-                <div>
-                  <div>
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                  <div>
-                    <DishDetail selectedDish={this.state.selectedDish} />
-                  </div>
                 </div>
             </div>
         );
