9ed1c3cc1c76efdbe64658142bf3b5fc652670c1
Morganhtrotter <morganhtrotter@gmail.com>
2020-04-18 17:59:22 -0700

    Start of week 2 assignment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -34,16 +34,16 @@ import { Link } from 'react-router-dom';
 	function RenderDish({dish}) {
 		if (dish != null) {
 			return(
-					<div className="col-12 col-md-5 m-1">
-						<Card key={dish.id} >
-				        <CardImg top src={dish.image} alt={dish.name} />
-				        <CardBody>
-				          <CardTitle>{dish.name}</CardTitle>
-				          <CardText>{dish.description}</CardText>
-				        </CardBody>
-				      </Card>
-			      	</div>
-	      	);
+				<div className="col-12 col-md-5 m-1">
+					<Card key={dish.id} >
+				    <CardImg top src={dish.image} alt={dish.name} />
+			      <CardBody>
+			        <CardTitle>{dish.name}</CardTitle>
+			        <CardText>{dish.description}</CardText>
+			      </CardBody>
+				  </Card>
+			  </div>
+	    );
 		} else {
 			return(
 				<div></div>
@@ -51,35 +51,33 @@ import { Link } from 'react-router-dom';
 		}
 	}
 
-	function RenderComments({comments}) {
-      console.log(comments);
-      var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-      if (comments != null)
-          if (comments.comment != null) {
-            const listComments = comments.map((comment) => 
-              <li>{comments.comment}<br /><br />{"--" + comments.comment 
-                  + " , "
-                  + months[(new Date(comments.date)).getMonth()] + " "
-                  + (new Date(comments.date)).getDate() + ", "
-                  + (new Date(comments.date)).getFullYear()}<br /><br /></li>
-            );
-            return(
-              <div className="col-12 col-md-5 m-1">
-                <h4>Comments</h4>
-                  <ul className="list-unstyled">{listComments}</ul>
-              </div>
-            );
-          } else {
-            return(
-              <div></div>
-            );
-          }
-      else
-          return(
-            <div></div>
-          );
-    }
+	function RenderComments({ comments }) {
+  const commentList = comments.map((comment) => {
+    return (
+      <ul key={comment.id} className="list-unstyled">
+        <li>{comment.comment}</li>
+        <li>
+          --{comment.author} ,{" "}
+          {new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit"
+          }).format(new Date(comment.date))}
+        </li>
+      </ul>
+    );
+  });
+  if (comments != null) {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
+        {commentList}
+      </div>
+    );
+  } else {
+    return <div />;
+  }
+}
 
 
 export default DishDetail;
