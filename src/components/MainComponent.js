1add084be86d1c4fb9050942879bcca7eb17488e
Morganhtrotter <morganhtrotter@gmail.com>
2020-05-11 16:23:59 -0700

    task 1 of assignmet 4

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -26,7 +26,8 @@ const mapDispatchToProps = (dispatch) => ({
 	fetchDishes: () => {dispatch(fetchDishes())},
 	resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
 	fetchComments: () => {dispatch(fetchComments())},
-	fetchPromos: () => {dispatch(fetchPromos())}
+	fetchPromos: () => {dispatch(fetchPromos())},
+	fetchLeaders: () => {dispatch(fetchLeaders())}
 });
 
 class Main extends Component {
@@ -38,11 +39,14 @@ class Main extends Component {
 		this.props.fetchDishes();
 		this.props.fetchComments();
 		this.props.fetchPromos();
+		this.props.fetchLeaders();
 	}
 
 	render() {
 
 		const HomePage = () => {
+			console.log(this.props.leaders);
+			console.log(this.props.dishes);
 			return(
 				<Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
 					dishesLoading={this.props.dishes.isLoading}
@@ -50,7 +54,9 @@ class Main extends Component {
 					promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
 					promosLoading={this.props.promotions.isLoading}
 					promosErrMess={this.props.promotions.errMess}
-					leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+					leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+					leadersLoading={this.props.leaders.isLoading}
+					leadersErrMess={this.props.leaders.errMess}
 				/>
 			);
 		}
